24af2b85b816412ddc033e9ec84ce667cc2466dc
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-10-18 21:36:43 +0300

    refactored  app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Work!')
+const cols = document.querySelectorAll('.col')
+
+const generateRandomColor = () => {
+  const hexCodes = '01234567ABCDEF'
+    let color = ''
+    for (let i =0; i < 6; i ++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    console.log(color)
+    return '#' + color
+}
+
+const setRandomColors = () => {
+    cols.forEach( col => {
+        const text = col.querySelector('h2')
+        const color = generateRandomColor()
+        col.style.background = color
+        text.textContent = color
+
+    })
+}
+
+setRandomColors()
