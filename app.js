cd3d20a2dc068b5534045e0190cd5310f4d617bd
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-10-19 14:59:23 +0300

    refactored  app.js: color of text and buttons

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const cols = document.querySelectorAll('.col')
 
-const generateRandomColor = () => {
-  const hexCodes = '01234567ABCDEF'
-    let color = ''
-    for (let i =0; i < 6; i ++) {
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-    }
-    console.log(color)
-    return '#' + color
-}
+// const generateRandomColor = () => {
+//   const hexCodes = '01234567ABCDEF'
+//     let color = ''
+//     for (let i =0; i < 6; i ++) {
+//         color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+//     }
+//     console.log(color)
+//     return '#' + color
+// }
 
 const setRandomColors = () => {
     cols.forEach( col => {
         const text = col.querySelector('h2')
-        const color = generateRandomColor()
+        const button = col.querySelector('button')
+        // const color = generateRandomColor()
+        const color = chroma.random()
+
         col.style.background = color
         text.textContent = color
 
+        setTextColor(text, color)
+        setTextColor(button, color)
     })
+}
+
+const setTextColor = (text, color) => {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 setRandomColors()
